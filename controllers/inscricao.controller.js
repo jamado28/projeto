5b2bae6a8ea89ff72c9a18321d2affb5780bcd7a
jamado28 <jose.simoes.amado@gmail.com>
2026-04-09 15:23:32 +0100

    Alteração

--- controllers/inscricao.controller.js
+++ controllers/inscricao.controller.js
@@ -4,7 +4,7 @@ const bilheteModel = require("../models/bilhete.model");
 exports.createInscricao = (req, res) => {
   const inscricao = inscricaoModel.create(req.body);
 
-  // 🔥 cria bilhete automaticamente
+  // cria bilhete automaticamente
   const bilhete = bilheteModel.create({
     inscricaoId: inscricao.id,
     tipo: inscricao.tipo
