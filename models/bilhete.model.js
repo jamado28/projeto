252e5fa62a3de67d10323f4cd02fe1fcbf193074
jamado28 <jose.simoes.amado@gmail.com>
2026-04-09 18:20:22 +0100

    Models Feitos

--- models/bilhete.model.js
+++ models/bilhete.model.js
@@ -1,9 +1,49 @@
-let bilhetes = [];
+const sequelize = require("sequelize");
+const conexao = require("../config/database");
+const Pessoa = require("./pessoa.model");
+const Evento = require("./evento.model");
 
-module.exports = {
-  create: (bilhete) => {
-    bilhete.id = bilhetes.length + 1;
-    bilhetes.push(bilhete);
-    return bilhete;
+const Bilhete = conexao.define(
+  "bilhete",
+  {
+    id_bilhete: {
+      type: sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    ano_bilhete: {
+      type: sequelize.INTEGER,
+      allowNull: false,
+    },
+    pessoa_nif: {
+      type: sequelize.INTEGER,
+      allowNull: false,
+    },
+    id_evento: {
+      type: sequelize.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "bilhete",
+    timestamps: true,
+    freezeTableName: true
   }
-};
+);
+
+
+// muitos bilhetes - 1 pessoa
+Bilhete.belongsTo(Pessoa, {
+  foreignKey: "pessoa_nif",
+  targetKey: "nif",
+  as: "pessoa",
+});
+
+// muitos bilhetes - 1 evento
+Bilhete.belongsTo(Evento, {
+  foreignKey: "id_evento",
+  targetKey: "id_evento",
+  as: "evento",
+});
+
+module.exports = Bilhete;
--- models/pagamento.model.js
+++ models/pagamento.model.js
@@ -1,9 +1,45 @@
-let pagamentos = [];
+const sequelize = require("sequelize");
+const conexao = require("../config/database");
+const Bilhete = require("./bilhete.model");
 
-module.exports = {
-  create: (pagamento) => {
-    pagamento.id = pagamentos.length + 1;
-    pagamentos.push(pagamento);
-    return pagamento;
+const Pagamento = conexao.define(
+  "pagamento",
+  {
+    id_pagamento: {
+      type: sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    iban: {
+      type: sequelize.STRING(34),
+      allowNull: false
+    },
+    preco: {
+      type: sequelize.DECIMAL(8, 2),
+      allowNull: false
+    },
+    estado: {
+      type: sequelize.BOOLEAN,
+      allowNull: false
+    },
+    id_bilhete: {
+      type: sequelize.INTEGER,
+      allowNull: false,
+      unique: true
+    },
+  },
+  {
+    tableName: "pagamento",
+    timestamps: true,
+    freezeTableName: true
   }
-};
+);
+
+// pagamento - pertence a um bilhete
+Pagamento.belongsTo(Bilhete, {
+  foreignKey: "id_bilhete",
+  targetKey: "id_bilhete",
+  as: "bilhete",
+});
+
+module.exports = Pagamento;
